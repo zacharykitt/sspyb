39d5b5ec9f646cf35bcacf7f2cfc6887b8481bc2
zacharykitt <g52@emailliame.com>
2018-10-14 19:26:57 -0400

    adding basic theme

--- sspyb/app.py
+++ sspyb/app.py
@@ -4,6 +4,8 @@ import yaml
 
 from flask import abort, Flask, render_template
 
+theme_dir = 'basic/'
+
 app = Flask(__name__)
 
 def aggregate_posts():
@@ -15,11 +17,11 @@ def aggregate_posts():
 
 @app.route('/')
 def index():
-    return render_template('index.html', posts=aggregate_posts())
+    return render_template(theme_dir + 'index.html', posts=aggregate_posts())
 
 @app.route('/posts/<slug>')
-def read(slug):
+def post(slug):
     for post in aggregate_posts():
         if post['slug'] == slug:
-            return render_template('post.html', post=post)
+            return render_template(theme_dir + 'post.html', post=post)
     abort(404)
